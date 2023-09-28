ba34784878d7bfac5da9138d711a4e060e189c27
mihnea112 <ayanna.mihnea@yahoo.com>
2023-09-28 18:26:40 +0300

    Ecorobo + RobotNokia done

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/RobotNokia.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/RobotNokia.java
@@ -6,17 +6,41 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 
 
 
-@TeleOp(group = "1. Robot Nokia")
+@TeleOp(name = "1. Robot Nokia")
 public class RobotNokia extends LinearOpMode {
     private DcMotor right, left;
+    boolean spate=false,slow=false;
     @Override
     public void runOpMode() throws InterruptedException {
         right= hardwareMap.get(DcMotor.class, "dr");
         left= hardwareMap.get(DcMotor.class, "st");
         waitForStart();
         while (!isStopRequested()) {
-            right.setPower(gamepad1.right_trigger/2);
-            left.setPower(-gamepad1.left_trigger/2);
+            if (slow == false) {
+                if (spate == false) {
+                    right.setPower(-gamepad1.right_trigger);
+                    left.setPower(gamepad1.left_trigger);
+                } else {
+                    right.setPower(gamepad1.right_trigger);
+                    left.setPower(-gamepad1.left_trigger);
+                }
+            } else {
+                if (spate == false) {
+                    right.setPower(-gamepad1.right_trigger / 2);
+                    left.setPower(gamepad1.left_trigger / 2);
+                } else {
+                    right.setPower(gamepad1.right_trigger / 2);
+                    left.setPower(-gamepad1.left_trigger / 2);
+                }
+            }
+            if (gamepad1.a) {
+                spate ^= true;
+                sleep(200);
+            }
+            if(gamepad1.b){
+                slow^=true;
+                sleep(200);
+            }
+            }
         }
     }
-}
